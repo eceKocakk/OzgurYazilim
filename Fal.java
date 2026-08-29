b18a93641a6a8061d19a1f4dd201f80db81005c2
sumeyyedrk <sumeyyeedurak@gmail.com>
2026-08-29 14:44:33 +0300

    Add files via upload

---
+++ Fal.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Fal {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int sayı;
+        System.out.print("Lütfen 1-10 arasında bir sayı giriniz:");
+        sayı=scan.nextInt();
+        if(sayı==1){
+
+        }
+        else if(sayı==2){
+
+        }
+        else if(sayı==3){
+
+        }
+        else if(sayı==4){
+
+        }
+        else if(sayı==5){
+
+        }
+        else if(sayı==6){
+
+        }
+        else if(sayı==6){
+
+        }
+        else if(sayı==7){
+
+        }
+        else if(sayı==8){
+
+        }
+        else if(sayı==9){
+
+        }
+        else if(sayı==10){
+
+        }else{
+            
+        }
+
+    }
+}
