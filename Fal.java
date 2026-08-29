35f3d4fa7046e3b99897a059c64302abcb6c6aa3
eceKocakk <ecee.kocakk5@gmail.com>
2026-08-29 15:06:14 +0300

    Update Fal.java

--- Fal.java
+++ Fal.java
@@ -9,7 +9,7 @@ public class Fal {
           System.out.println("3 vakte kadar iyi bir haber alacaksın.");
         }
         else if(sayı==2){
-
+         System.out.println("Kariyerinde bir değişiklik yaşayacaksın.");
         }
         else if(sayı==3){
 
